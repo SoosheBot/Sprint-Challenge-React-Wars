c4326d9a5b87e45df6f041f3f70eeb26c990867c
sooshe <sooshe@sooshebot.com>
2019-09-13 13:44:01 -0500

    completed mvp

--- starwars/src/components/SWData.js
+++ starwars/src/components/SWData.js
@@ -5,33 +5,35 @@ import axios from "axios";
 
 export default function SWData() {
     const [people, setPeople] = useState([]);
-    
     useEffect(() => {
         axios
             .get(`https://swapi.co/api/people/`)
             .then(response => {
-            console.log("Power to the people", response);
-            setPeople(response.data);
+                console.log("Power to the people", response);
+                setPeople(response.data.results);
+     
     })
         .catch(error => {
             console.log("Not the droids...people you seek", error);
         });
 
-    }, []);
+    },[]);
 
     return (
-        <div className="sw-info">
-            {people.map(info => {
-                return(
-                    <SWCard key={info}>
-                        name={info}
-                        
-                    </SWCard>
+        <div className="sw-information">
+            {people.map(swInfo => {
+                return (
+                    <SWCard key={swInfo.id}
+                        name={swInfo.name}
+                        films={swInfo.films.join('\r\n')}
+                        birth_year={swInfo.birth_year}
+                        height={swInfo.height}
+                        mass={swInfo.mass}
+                        hair={swInfo.hair}
+                    />
                 );
-
-            })};
-                
-
+                })}       
         </div>
+   
     );
 }
